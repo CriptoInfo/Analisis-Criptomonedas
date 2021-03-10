c2fcf36cc8a333324186b2d1b4f978175adf4cbf
migue0418 <55736829+migue0418@users.noreply.github.com>
2021-03-10 17:40:11 +0100

    Upload GO files

---
+++ src/cryptogo.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	coingecko "github.com/superoo7/go-gecko/v3"
+)
+
+func main() {
+	httpClient := &http.Client{
+		Timeout: time.Second * 10,
+	}
+	CG := coingecko.NewClient(httpClient)
+
+	fmt.Println(CG.SimpleSinglePrice("bitcoin", "eur"))
+}
